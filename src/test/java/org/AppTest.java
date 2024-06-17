a8762a34e3edae9691f2d60ac7ffbc2ad3dbc3bd
Roman-Maliarenko <romamalarenko1@gmail.com>
2024-06-17 13:06:07 +0300

    txt

--- src/test/java/org/AppTest.java
+++ src/test/java/org/AppTest.java
@@ -1,14 +1,17 @@
+
+
 package org;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
