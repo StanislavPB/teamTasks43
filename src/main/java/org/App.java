cf57f8560f67a6261f33710f9259d60820cd2289
Roman-Maliarenko <romamalarenko1@gmail.com>
2024-06-18 20:41:46 +0300

    add services

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -11,12 +11,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        ProjectRepository repository = new ProjectRepository();
-        ProjectService service = new ProjectService(repository);
-
-        Project project1 = new Project(12L,"Project1","easy");
-        Project project2 = new Project(1L,"Project2","hard");
-        Project project3 = new Project(13L,"Project3","easy");
 
 
 
